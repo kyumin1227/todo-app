4d96a8ca7ace793a38ec1e3df6cdc5cd083b1bd2
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-08-17 13:55:45 +0900

    Selectors 1

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,24 +2,39 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoState } from "./atoms";
+import { toDoSelector, toDoState } from "./atoms";
 import ToDo from "./ToDo";
 
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoState);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
   
   console.log(toDos);
+  console.log(toDo);
+  
 
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-        <CreateToDo /> 
+      <CreateToDo /> 
+      <h2>To Do</h2>
       <ul>
-        {toDos.map(toDo => <ToDo {...toDo} />)}
+        {toDo.map(toDo => <ToDo {...toDo} />)}
       </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map(toDo => <ToDo {...toDo} />)}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map(toDo => <ToDo {...toDo} />)}
+      </ul>
+      <hr />
     </div>
   )
 }
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   text: string;
@@ -9,4 +9,18 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+})
+
+// Selector는 atom을 받아서 변형
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    // filter를 통해 카테고리별로 구분
+    return [
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE"),
+    ];
+  }
 })
